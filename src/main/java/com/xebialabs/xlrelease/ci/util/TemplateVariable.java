a235fba678a58cb95d75e31835043eee70d09cf6
saraswat2385 <saraswat2385@gmail.com>
2016-12-08 16:31:13 +0530

    small fix

--- src/main/java/com/xebialabs/xlrelease/ci/util/TemplateVariable.java
+++ src/main/java/com/xebialabs/xlrelease/ci/util/TemplateVariable.java
@@ -1,21 +1,21 @@
 /**
  * Copyright (c) 2014, XebiaLabs B.V., All rights reserved.
- *
- *
+ * <p/>
+ * <p/>
  * The XL Release plugin for Jenkins is licensed under the terms of the GPLv2
  * <http://www.gnu.org/licenses/old-licenses/gpl-2.0.html>, like most XebiaLabs Libraries.
  * There are special exceptions to the terms and conditions of the GPLv2 as it is applied to
  * this software, see the FLOSS License Exception
  * <https://github.com/jenkinsci/xlrelease-plugin/blob/master/LICENSE>.
- *
+ * <p/>
  * This program is free software; you can redistribute it and/or modify it under the terms
  * of the GNU General Public License as published by the Free Software Foundation; version 2
  * of the License.
- *
+ * <p/>
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
  * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- *
+ * <p/>
  * You should have received a copy of the GNU General Public License along with this
  * program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth
  * Floor, Boston, MA 02110-1301  USA
@@ -29,6 +29,9 @@ import javax.xml.bind.annotation.XmlRootElement;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.xebialabs.xlrelease.ci.NameValuePair.VARIABLE_PREFIX;
+import static com.xebialabs.xlrelease.ci.NameValuePair.VARIABLE_SUFFIX;
 
 @XmlRootElement
 @JsonIgnoreProperties(ignoreUnknown = true)
@@ -55,7 +58,10 @@ public class TemplateVariable {
     }
 
     private static String getVariableName(String variable) {
-        return variable.substring(2, variable.length() - 1);
+        if (variable.startsWith(VARIABLE_PREFIX) && variable.endsWith(VARIABLE_SUFFIX)) {
+            variable = variable.substring(2, variable.length() - 1);
+        }
+        return variable;
     }
 
     public String getKey() {
